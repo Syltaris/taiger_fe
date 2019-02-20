d1f2d290baa618a556cbdc2d3daf36c4989a656b
zexuan <syltaris@gmail.com>
2019-02-20 20:26:12 +0800

    fetch github repos and display names

--- gh/src/components/Page.vue
+++ gh/src/components/Page.vue
@@ -1,6 +1,9 @@
 <template>
     <div class='container'>
         <h1>{{data.name}}</h1>
+        <li v-for="(item, index) in repos" :key="item.name">
+            {{ index }} - {{ item.name }}
+        </li>
     </div>
 </template>
 
@@ -9,6 +12,29 @@ export default {
     name: 'Page', 
     props: {
         data: Object
+    },
+    data() {
+        return {
+            repos: [],
+        }
+    },
+    methods: {
+        fetchGhDetails() {
+            fetch(`https://api.github.com/${this.data.name}`)
+            .then(res => this.body = res.body)
+        }
+    },
+    watch: {
+        data: {
+            immediate: true,
+            deep: true,
+            handler: function(newVal) { // watch it
+                this.data = newVal
+                fetch(`https://api.github.com/users/${this.data.name}/repos`)
+                .then(res => res.json())
+                .then(json => this.repos = json)
+            }
+        }
     }
 }
 </script>
